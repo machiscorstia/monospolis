61408db7dd9d403046e88d840f8d14d941a00745
Manuel Cabral <cabral.manuel@yandex.com>
2021-02-04 10:02:53 -0300

    metodos para manipular el texto

--- src/interfaz/boton.py
+++ src/interfaz/boton.py
@@ -2,7 +2,7 @@ import pygame.font
 from src.constantes import *
 
 class Boton:
-    def __init__(self, x, y, fuente=FUENTE_PRINCIPAL, tamaniof=TF_NORMAL, grueso=False, colort=BLANCO, bg=NEGRO, escala=ESCALA_NORMAL, m=None, input=False, acciones=False):
+    def __init__(self, x, y, fuente=FUENTE_PRINCIPAL, tamaniof=TF_NORMAL, grueso=False, colort=BLANCO, bg=NEGRO, escala=ESCALA_NORMAL, m=None, dinamico=False, input=False, acciones=False):
         self.x, self.y = x, y
         self.anchura, self.altura = escala
         self.fuente = pygame.font.SysFont(fuente, tamaniof, bold=False)
@@ -12,33 +12,41 @@ class Boton:
         self.colorTexto = colort
         self.rect = pygame.Rect(self.x, self.y, self.anchura, self.altura)
         self.acciones = acciones
-        self.mensaje = m
+        self.mensaje = str(m) if type(m) == int else m
+        self.dinamico = dinamico
         self.prepararMensaje(self.mensaje)
     
-    def prepararMensaje(self, mensaje):
-        self.imagenMensaje = self.fuente.render(mensaje, True, self.colorTexto, self.fondo)
+    def prepararMensaje(self, m):
+        self.imagenMensaje = self.fuente.render(m, True, self.colorTexto, self.fondo)
         self.imagenMensajeRect = self.imagenMensaje.get_rect()
         self.imagenMensajeRect.center = self.rect.center
 
+    def cambiarFondo(self, fondo): self.fondo = fondo
+    
     def cambiarColor(self, colorTexto, colorFondo):
         self.imagenMensaje = self.fuente.render(self.mensaje, True, colorTexto, colorFondo)
         self.fondo = colorFondo
 
-    def cambiarTexto(self, m):
-        self.mensaje = m
-    
-    def estaColiccionando(self, punto):
-        return self.rect.collidepoint(punto)
+    def mensajeNulo(self): return self.mensaje
 
-    def agregarAccion(self, accion):
-        print(accion, 'agregando')
-        self.acciones.append(accion)
+    def agregarCaracter(self, caracter): self.mensaje += str(caracter)
+    
+    def eliminarCaracter(self): self.mensaje = self.mensaje[:-1]
+    
+    def cambiarTexto(self, m): self.mensaje = str(m)
+    
+    def estaColiccionando(self, punto): return self.rect.collidepoint(punto)
+
+    def agregarAccion(self, accion): self.acciones.append(accion)
     
     def ejecutarAcciones(self):
         if not self.acciones: return False 
         else: 
-            for accion in self.acciones: accion()
+            for accion in self.acciones: 
+                accion = accion()
+                if accion: self.mensaje = str(accion) if type(accion) == int else accion
     
     def mostrar(self, pantalla):
+        if self.dinamico: self.prepararMensaje(self.mensaje)
         pantalla.fill(self.fondo, self.rect)
         pantalla.blit(self.imagenMensaje, self.imagenMensajeRect)
--- src/interfaz/texto.py
+++ src/interfaz/texto.py
@@ -2,21 +2,31 @@ import pygame.font
 from src.constantes import *
 
 class Texto:
-    def __init__(self, x, y, fuente=FUENTE_PRINCIPAL, grueso=False, tamaniof=TF_NORMAL, colort=BLANCO, escala=ESCALA_NORMAL, m=None):
+    def __init__(self, x, y, fuente=FUENTE_PRINCIPAL, grueso=False, tamaniof=TF_NORMAL, bg=NEGRO, colort=BLANCO, escala=ESCALA_NORMAL, m=None, dinamico=True):
         self.x, self.y = x, y
         self.anchura, self.altura = escala
         self.fuente = pygame.font.SysFont(fuente, tamaniof, bold= grueso)
-        self.colorFondo = (0,0,0,0)
+        self.colorFondo = bg
         self.colorTexto = colort
         self.rect = pygame.Rect(self.x, self.y, self.anchura, self.altura)
         self.mensaje = m
+        self.dinamico = dinamico
+        self.caracterLimite = None
         self.prepararMensaje(self.mensaje)
     
+    def cambiarTexto(self, m): self.mensaje = str(m)
+    
+    def agregarCaracter(self, caracter): self.mensaje += str(caracter)
+    
+    def eliminarCaracter(self): 
+        if self.mensaje[len(self.mensaje) - 1] != self.caracterLimite: self.mensaje = self.mensaje[:-1]
+
     def prepararMensaje(self, mensaje):
         self.mensajeImagen = self.fuente.render(mensaje, True, self.colorTexto, self.colorFondo)
         self.mensajeImagenRect = self.mensajeImagen.get_rect()
         self.mensajeImagenRect.center = self.rect.center
 
     def mostrar(self, pantalla):
+        if self.dinamico: self.prepararMensaje(self.mensaje)
         self.mensajeImagen.set_colorkey(self.colorFondo)
         pantalla.blit(self.mensajeImagen, self.mensajeImagenRect)
