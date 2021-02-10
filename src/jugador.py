7608627f66a1ec23a67dcff34ed9be7c5a61c88c
Manuel Cabral <cabral.manuel@yandex.com>
2021-02-10 13:37:36 -0300

    color de la ficha

--- src/jugador.py
+++ src/jugador.py
@@ -1,3 +1,4 @@
+import pygame as py
 from src.constantes import *
 from src.interfaz.graficos import *
 
@@ -8,9 +9,15 @@ class Jugador:
         self.posicion = nombre
         self.imagen = cargarImagenFicha()
         self.moviendose = False
+        self.colorFicha = py.Surface(self.imagen.get_size()).convert_alpha()
+        self.colorFicha.fill(color)
+        self.imagen.blit(self.colorFicha, (0,0), special_flags= py.BLEND_RGBA_MULT)
         self.rect = self.imagen.get_rect()
         self.rect.x, self.rect.y = posicion
 
-    def establecerPosicion(self, posicion): self.rect.x, self.rect.y = posicion
+    def comenzarMovimiento(self, posicionInicio, posicionFinal):
+        pass
+    
+    def establecerPosicion(self, posicion): self.rect.centerx, self.rect.centery = posicion
 
     def mostrar(self, pantalla): pantalla.blit(self.imagen, self.rect)
