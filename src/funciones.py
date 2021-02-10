3416710db8b8a84427b3a2ef924710b84e8d2293
Manuel Cabral <cabral.manuel@yandex.com>
2021-02-10 13:36:15 -0300

    fix  posiciones de casillas

--- src/funciones.py
+++ src/funciones.py
@@ -5,7 +5,8 @@ def crearRutaVertical(yi, yf, x, s):
 
 def crearRutaHorizontal(xi, xf, y, s):
     ruta = list()
-    for x in range(xi, xf, s): ruta.append([x, y])
+    for x in range(xi, xf, s):
+        ruta.append([x, y])
     return ruta
 
 def siguienteItem(valorActual, lista):
@@ -16,8 +17,8 @@ def siguienteItem(valorActual, lista):
     
 def generarPosicionesTablero():
     ruta = list()
-    for posicion in crearRutaVertical(750, 0, x=40, s=-100): ruta.append(posicion)
-    for posicion in crearRutaHorizontal(100, 800, y=40, s=120): ruta.append(posicion)
-    for posicion in crearRutaVertical(50, 800, x=750, s=50): ruta.append(posicion)
-    for posicion in crearRutaHorizontal(800, 50, y=750, s=-50): ruta.append(posicion)
+    for posicion in crearRutaVertical(750, 0, x=50, s=-100): ruta.append(posicion)
+    for posicion in crearRutaHorizontal(150, 800, y=50, s=100): ruta.append(posicion)
+    for posicion in crearRutaVertical(150, 800, x=750, s=100): ruta.append(posicion)
+    for posicion in crearRutaHorizontal(650, 50, y=750, s=-100): ruta.append(posicion)
     return ruta
