a169802a8af751526f397988ea048278096282b9
Manuel Cabral <cabral.manuel@yandex.com>
2021-02-10 13:38:15 -0300

    metodo tirar dados agregado y turno de jugador

--- src/tablero.py
+++ src/tablero.py
@@ -1,15 +1,20 @@
 from src.funciones import *
 from src.constantes import *
+from random import randrange
 
 class Tablero:
     def __init__(self):
         self.jugadores = list()
+        self.jugadorMoviendose = None
         self.dineroInicial = DINERO_INICIAL
         self.salario = CANTIDAD_SALARIOS[SALARIO_PREDETERMINADO]
         self.netoMaximo = CANTIDAD_NETOS[NETO_PREDETERMINADO]
         self.cantidadJugadores = CANTIDAD_JUGADORES[JUGADORES_PREDETERMINADO]
         self.posicionCasillas = generarPosicionesTablero()
+        self.turnoJugador = None
         self.contador = 0
+
+    def establecerTurnoInicial(self): self.jugadorConTurno = self.jugadores[0]
 
     def cambiarCantidadJugadores(self):
         self.cantidadJugadores = CANTIDAD_JUGADORES[siguienteItem(self.cantidadJugadores, CANTIDAD_JUGADORES)]
@@ -28,6 +33,9 @@ class Tablero:
         self.netoMaximo = CANTIDAD_NETOS[siguienteItem(self.netoMaximo, CANTIDAD_NETOS)]
         return self.netoMaximo
     
+    def tirarDados(self):
+        return randrange(6)
+        
     def mostrarPosiciones(self):
         return self.posicionCasillas
     
