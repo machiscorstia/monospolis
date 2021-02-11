2815e8112277720cd948daca4e0ce41fd9aa7522
Manuel Cabral <cabral.manuel@yandex.com>
2021-02-11 19:18:54 -0300

    generador de posiciones (precio, nombre, propietario)

--- src/funciones.py
+++ src/funciones.py
@@ -14,7 +14,46 @@ def siguienteItem(valorActual, lista):
     if index < len(lista) - 1: index += 1
     else: index = 0
     return index
-    
+
+def verificarExistencia(numero, lista):
+    existe = False
+    for i in lista:
+        if numero == i: existe = True
+    return existe
+        
+def generarPosicionPrecios():
+    ruta = list()
+    temp = None
+    for i, r in enumerate(generarPosicionesTablero()):
+        if i < 7: temp = [r[0]-15, r[1]- 40]
+        elif i > 7 and i < 14: temp = [r[0], r[1]-35]
+        elif i > 14 and i < 21: temp = [r[0], r[1]-35]
+        else: temp = [r[0]+15, r[1]-35]
+        ruta.append(temp)
+    return ruta   
+
+def generarPosicionPropietario():
+    ruta = list()
+    temp = None
+    for i, r in enumerate(generarPosicionesTablero()):
+        if i < 7: temp = [r[0]-40, r[1]- 35]
+        elif i > 7 and i < 14: temp = [r[0]+20, r[1]-35]
+        elif i > 14 and i < 21: temp = [r[0]+20, r[1]-35]
+        else: temp = [r[0]+15, r[1]-35]
+        ruta.append(temp)
+    return ruta   
+
+def generarPosicionCiudades():
+    ruta = list()
+    temp = None
+    for i, r in enumerate(generarPosicionesTablero()):
+        if i < 7: temp = [r[0]-10, r[1]+18]
+        elif i > 7 and i < 14: temp = [r[0]-5, r[1]+18]
+        elif i > 14 and i < 21: temp = [r[0]-10, r[1]+18]
+        else: temp = [r[0]-10, r[1]+18]
+        ruta.append(temp)
+    return ruta
+
 def generarPosicionesTablero():
     ruta = list()
     for posicion in crearRutaVertical(750, 0, x=50, s=-100): ruta.append(posicion)
